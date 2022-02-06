0d716c2215e752e63febf7c9f2830d283d6b7f7a
Crussader <75786691+Crussader@users.noreply.github.com>
2022-02-06 21:25:44 +0400

    Add files via upload

---
+++ Main/main.py
@@ -0,0 +1,13 @@
+from core import *
+from core.utils import *
+
+from customtkinter import *
+from typing import Dict
+
+class Main:
+
+    APPNAME = "Caduceus Hospital"
+    frames: Dict[str, CTkFrame]= {}
+
+    def __init__(self, user):
+        pass
