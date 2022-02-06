42d0c96a1ffea6e42de09ca71bc9b1fd5f085101
Crussader <75786691+Crussader@users.noreply.github.com>
2022-02-06 22:49:11 +0400

    working database code

--- Main/core/database.py
+++ Main/core/database.py
@@ -7,6 +7,24 @@ __all__ = ('Database',)
 
 MYSQL = mysql.MySQLConnection
 
+def exists(table=True, kwargs=True): # checks if table or kwargs is not empty when calling a function
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            self, *other = args
+            if table:
+                if not (name:=kwargs.get('table')):
+                    raise ValueError("No Table Name given")
+                else:
+                    kwargs.update({'table': name or self.table})
+            
+            if kwargs:
+                if not kwargs.get('kwargs'):
+                    raise ValueError("No Arguments given to add to Database")
+
+            return func(self, *other)
+        return inner
+    return wrapper
+
 class Database:
 
     def __init__(self, from_config=True, **kwargs):
@@ -19,14 +37,16 @@ class Database:
         else:
             config = kwargs
         
+        self._database = config.get('database', '')
+        self._table = config.pop('table', '')
         self.cnx: MYSQL = mysql.connect(**config)
         self.cursor: MySQLCursor = self.cnx.cursor()
-
-        self._database = config["database"]
-        self._table = ""
+        
+        if self.is_connected():
+            print("connected!")
 
     @property
-    def table(self):
+    def table(self) -> str:
         return self._table
     
     @table.setter
@@ -37,27 +57,35 @@ class Database:
         self._table = other
         return self._table
 
+    @property
+    def database(self) -> str:
+        return self._database
+    
+    @database.setter
+    def database(self, other: str) -> str:
+        if not isinstance(other, str):
+            raise TypeError("Database name must be a string")
+        
+        if other == self._database:
+            return self._database
+        
+        self.cursor.execute('use %s', (other, ))
+        self._database = other
+        return self._database
+        
     def is_connected(self):
         return self.cnx.is_connected()
 
-    def add_rec(self, table: Optional[str]='', **kwargs: Dict[str, str]) -> int:
-
-        table = table or self._table
-
-        if not table:
-            raise ValueError("No Table Name given")
-
-        if not kwargs:
-            raise ValueError("No Arguments given to add to Database")
-
+    @exists()
+    def add_rec(self, table: Optional[str]='', kwargs: Dict[str, str] = {}) -> int:
         keys = ', '.join(kwargs.keys())
         values = ', '.join(['%s'] * len(kwargs))
         sql = f"INSERT INTO {table} ({keys}) VALUES ({values})"
-        self.cursor.execute(sql, kwargs.values())
+        self.cursor.execute(sql, tuple(kwargs.values()))
         self.cnx.commit()
     
+    @exists(kwargs=False)
     def find_rec(self, search: Union[str, int], filter_by: str, table: Optional[str]=''):
-        
         search_params = {
             "ID": "select * from {} where id = %s".format(table),
             "AGE": "select * from {} where age = %s".format(table),
@@ -66,57 +94,55 @@ class Database:
             "DOCTOR_IN_CHARGE": "select * from {} where doctor_in_charge = %s".format(table),
             "DEPARTMENT": "select * from {} where department = %s".format(table),
         }
-        table = table or self._table
-
-        if not table:
-            raise ValueError("No Table Name given")
         
-        if filter_by not in search_params.values():
+        if filter_by not in search_params.keys():
             raise ValueError("Invalid Filter")
 
         if isinstance(search, int):
-            search_params = search_params[0: 2]
+            search_params = {k: v for k, v in search_params.items() if k in ['ID', 'AGE']}
         elif isinstance(search, str):
-            search_params = search_params[2: ]
+            search_params = {k: v for k, v in search_params.items() if k not in ['ID', 'AGE']}
         else:
             raise TypeError("Search Parameter must be an int or str")
 
         results = []
         
-        for param in search_params in search_params:
+        for param in search_params.values():
             self.cursor.execute(param, (search,))
             if (result := self.cursor.fetchall()) not in [(), []]:
                 results.append(result)
-            
-    
-    def update_rec(self, id: str, table: Optional[str]='',**kwargs: Dict[str, str]):
-        table = table or self._table
-
-        if not table:
-            raise ValueError("No Table Name given")
         
-        if not kwargs:
-            raise ValueError("No Arguments given to update in Database")
+        return results
+    
+    @exists()
+    def update_rec(self, id: str, table: Optional[str]='', kwargs: Dict[str, str]={}):
+        update = ', '.join(f"{k}={v}" if isinstance(v, int) else f'{k}="{v}"'
+                           for k, v in kwargs.items())
 
-        keys = ', '.join(kwargs.keys())
-        values = ', '.join(['%s'] * len(kwargs))
-        sql = "UPDATE {} SET ({}) = ({}) where id = {}".format(table, keys, values, id)
-        self.cursor.execute(sql, kwargs.values())
-        self.cnx.commit()
+        sql = """UPDATE {} SET {} WHERE id = {}""".format(table, update, id)
+        self.cursor.execute(sql)
         if self.cursor.rowcount:
-            return True # check if the same row has the same values
-                        # because if it does then .rowcount will be 0
-                        # and that could be mean an error if not checked
+            self.cnx.commit() # commit only if there is rowcount
+                              # so that would mean there was a change
+                              # then we can commit it to mysql
+            return True
 
         else:
             return False
 
-    def delete_rec(self, id: int, verification_id: str):
-        pass        
+    @exists(kwargs=False)
+    def delete_rec(self, id: int, verification_id: str, table: Optional[str]=''):
+        # check verification_id with the one the computer generated and the user recieved
+        sql = "delete from {} where id={}".format(table, id)
 
     def close(self):
         self.cnx.close()
 
 if __name__ == '__main__':
-    db = Database()
+    db = Database(from_config=False, host="localhost", password='panipuri123*', user='root', database='hospital')
+    # db.add_rec('paitents', {'id': 1, 'first_name': 'sai', 'last_name': 'krishina'})
+    db.update_rec(1, 'paitents', {'gender': 'male', 'age': 20, 'doctor_in_charge': 123})
+    a = db.find_rec(1, "ID", 'paitents')
+    print(a)
+
     # print(db)
