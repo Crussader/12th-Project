b12498f9ebcafa83b033d1064affbdd16ec2b206
Crussader <75786691+Crussader@users.noreply.github.com>
2022-02-16 13:19:28 +0400

    Add files via upload

--- Main/main.py
+++ Main/main.py
@@ -10,17 +10,19 @@ class Main(CTk):
     APPNAME = "Caduceus Hospital"
     frames: Dict[str, CTkFrame] = {}
     current = None
+    panel: Panel = None
 
     def __init__(self, panel):
+
+        self.panel = panel(self)
 
         super().__init__(self.APPNAME)
         self.state('zoomed')
         self.configure(fg_color=None)
         load_theme(self)
         tksvg.load(self)
+        self.load_home()
 
-
-        self.load_home(panel)
 
     def _set_and_run(self, button: CTkButton, func: str):
         button.configure(fg_color=Color.LABEL_BG)
@@ -34,19 +36,7 @@ class Main(CTk):
         if i <= max:
             self.after(10, self._expand, frame, i+1)
 
-    def _shortcut(self, master: CTkFrame, image: str, text: str, sub_text: str):
-        frame = CTkFrame(master)
-        CTkFrame(frame, height=50, fg_color=Color.FRAME_2).pack(padx=80, pady=20)
-        short = CTkButton(frame, text=text, fg_color=Color.FRAME_2, 
-                          compound="top", image=get_image(image),
-                          hover=False, text_font=Defont.add(20))
-        short.pack(pady=5, padx=20)
-        CTkLabel(frame, text=sub_text, width=200, fg_color=Color.FRAME_2,
-                 text_font=Defont.add(11), text_color="lightgrey").pack(pady=10)
-        CTkFrame(frame, height=50, fg_color=Color.FRAME_2).pack(padx=80, pady=20)
-        frame.pack(side="left", padx=90)
-
-    def load_home(self, panel):
+    def load_home(self):
 
         tab = CTkFrame(self, width=70)
         self.frames['tab'] = tab
@@ -63,46 +53,6 @@ class Main(CTk):
         ttk.Separator(tab).pack(fill='x', padx=5, pady=10)
 
         CTkFrame(tab, fg_color=Color.FRAME, width=70, height=125).pack(pady=5) # seperator
-
-        home = CTkButton(tab, image=get_image('home.svg', wh=0.08), text='',
-                         fg_color=Color.LABEL_BG, width=70, height=50,
-                         hover_color=Color.LABEL_BG,
-                         command=lambda: self._set_and_run(home, 'home_page'))
-        self.current = home
-        home.pack(pady=20)
-
-
-        if panel in ["admin", "doctor"]:
-            add_user = CTkButton(tab, image=get_image("add-user.svg", wh=0.1), 
-                                text='', fg_color=Color.FRAME, width=70, height=60, 
-                                hover_color=Color.LABEL_BG, compound='right',
-                                command=lambda: self._set_and_run(add_user, "add_user"))
-            add_user.pack(pady=20)
-
-            find_user = CTkButton(tab, image=get_image("find-user.svg", wh=0.1),
-                                text='', fg_color=Color.FRAME, width=70, height=60, hover_color=Color.LABEL_BG,
-                                command=lambda: self._set_and_run(find_user, "find_user"))
-            find_user.pack(pady=20)
-
-        if panel in ["admin", "user", 'doctor']:
-            update_user = CTkButton(tab, image=get_image("update-user.svg", wh=0.1),
-                                text='', fg_color=Color.FRAME, width=70, height=60, hover_color=Color.LABEL_BG,
-                                command=lambda: self._set_and_run(update_user, "update_user"))
-            update_user.pack(pady=20)
-
-        if panel == "admin":
-            delete_user = CTkButton(tab, image=get_image("remove-user.svg", wh=0.1),
-                                    text='', fg_color=Color.FRAME, width=70, height=60, hover_color=Color.LABEL_BG, 
-                                    command=lambda: self._set_and_run(delete_user, "delete_user"))
-            delete_user.pack(pady=20)
-
-        CTkLabel(tab, text=f'V {__version__}', text_color=Color.LABEL_BG, 
-                 fg_color=None, width=50, text_font=Defont.add(10)).pack(side='bottom', pady=10)
-
-        settings = CTkButton(tab, image=get_image("settings.svg", wh=0.1),
-                             text='', fg_color=Color.FRAME, width=70, height=60, hover_color=Color.LABEL_BG,
-                             command=lambda: self._set_and_run(settings, "settings"))
-        settings.pack(pady=10, side='bottom')
 
 
         profile = CTkFrame(self, width=200, height=70)
@@ -124,15 +74,34 @@ class Main(CTk):
         # bind hover to logout
         log_out.place(relx=.34, rely=.73)
 
+        home = CTkButton(tab, image=get_image('home.svg', wh=0.08), text='',
+                         fg_color=Color.LABEL_BG, width=70, height=50,
+                         hover_color=Color.LABEL_BG,
+                         command=lambda: self._set_and_run(home, 'home_page'))
+        self.current = home
+        home.pack(pady=20)
+
+        self.panel.load_tab()
+
+        CTkLabel(tab, text=f'V {__version__}', text_color=Color.LABEL_BG, 
+                 fg_color=None, width=50, text_font=Defont.add(10)).pack(side='bottom', pady=10)
+
+
+        settings = CTkButton(tab, image=get_image("settings.svg", wh=0.1),
+                             text='', fg_color=Color.FRAME, width=70, height=60, hover_color=Color.LABEL_BG,
+                             command=lambda: self._set_and_run(settings, "settings"))
+        settings.pack(pady=10, side='bottom')
+
+
         profile.pack(side='right', anchor="ne", padx=10, pady=10, ipadx=70)
         # self.after(2000, self._expand, profile, 7)
 
-        self.home(panel)
+        self.home()
 
         tab.pack(anchor='w', fill="y", expand=True, 
                  side="left", pady=10, padx=10)
     
-    def home(self, panel):
+    def home(self):
         
         main_win = CTkFrame(self, width=500, height=500)
         self.frames['main'] = main_win
@@ -142,19 +111,25 @@ class Main(CTk):
                             fg_color=Color.FRAME)
         question.pack(pady=20)
 
-        if panel in ["admin", "doctor"]:
+        self.panel.home()
+        # if panel in ["admin", "doctor"]:
 
-            self.after(500, self._shortcut, main_win, 'add-user.svg',
-                       "\nAdd Patient", "Add a Patient to Database")
+        #     self.after(500, self._shortcut, main_win, 'add-user.svg',
+        #                "\nAdd Patient", "Add a Patient to Database")
 
-            self.after(800, self._shortcut, main_win, "find-user.svg",
-                       "\nFind Patient", "Find a Paitent from Database")
+        #     self.after(800, self._shortcut, main_win, "find-user.svg",
+        #                "\nFind Patient", "Find a Paitent from Database")
 
-            self.after(1000, self._shortcut, main_win, "update-user.svg",
-                       "\nUpdate Paitent", "Update Paitent Records")
+        #     self.after(1000, self._shortcut, main_win, "update-user.svg",
+        #                "\nUpdate Paitent", "Update Paitent Records")
 
         main_win.place(relx=0.1, rely=0.15,
                        relheight=0.8, relwidth=0.85)
 
 
-Main('admin').mainloop()
+
+# Main('admin').mainloop()
+if __name__ == "__main__":
+    # app = Main(User)
+    # app.mainloop()
+    loading_screen(None)
